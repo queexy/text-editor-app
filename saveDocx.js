e520f9bfb100769e73b57116d7d90f7cfade895a
queexy <93815379+queexy@users.noreply.github.com>
2024-11-20 22:33:10 +0100

    Update saveDocx.js

--- saveDocx.js
+++ saveDocx.js
@@ -1,15 +1,11 @@
 const { Document, Packer, Paragraph, TextRun } = require('docx');
 
-/**
- * Zamienia HTML na dokument docx
- * @param {string} htmlContent - Treść w HTML (z edytora)
- * @param {function} callback - Funkcja zwrotna z buforem
- */
+
 function saveAsDocx(htmlContent, callback) {
-  // Rozbij HTML na linie, ignorując tagi <br>
+
   const lines = htmlContent.split(/<div>|<\/div>|<br>/).filter((line) => line.trim() !== '');
 
-  // Tworzy akapity z treści
+  
   const paragraphs = lines.map((line) =>
     new Paragraph({
       children: [new TextRun(line)],
